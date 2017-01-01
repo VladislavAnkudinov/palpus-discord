7898ddddd3fa5da7d2eae392810f61d0d714928e
vladislav.ankudinov <vladislav.ankudinov@gmail.com>
2017-01-01 11:25:02 -0500

    listen jira

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 
 var app = express();
 app.use(bodyparser.urlencoded({extended: true}));
+app.use(bodyparser.json());
 
 var webhook = process.env.DISCORD_WEBHOOK;
 console.log('DISCORD_WEBHOOK =', webhook);
@@ -13,7 +14,24 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.post('/jira', (req, res) => {
+ //console.log('req.body =', req.body);
+ request({
+  method: 'POST',
+  url: webhook,
+  json: {
+    username: 'Jira listener',
+    content: 'something happen'
+  }}, (err, ret, body) => {
+    console.log('err =', err);
+    //console.log('ret =', ret);
+    if (err) res.error(err);
+    res.status(200).send(req.body);
+  });
+});
+
 app.post('/webhook', (req, res) => {
+  console.log('req =', req.body);
   request({
     method: 'POST',
     url: webhook,
