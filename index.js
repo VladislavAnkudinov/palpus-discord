e19716f9fe58f50d588cf419f1a8abc2dbc55388
Vladislav Ankudinov <vladislavankudinov@gmail.com>
2017-01-01 21:11:30 +0300

    track jira:issue_updated

--- index.js
+++ index.js
@@ -16,18 +16,48 @@ app.get('/', (req, res) => {
 
 app.post('/jira', (req, res) => {
  //console.log('req.body =', req.body);
- request({
-  method: 'POST',
-  url: webhook,
-  json: {
-    username: 'Jira listener',
-    content: 'something happen'
-  }}, (err, ret, body) => {
-    console.log('err =', err);
-    //console.log('ret =', ret);
-    if (err) res.error(err);
-    res.status(200).send(req.body);
-  });
+ if (req.body && req.body.webhookEvent == 'jira:issue_updated') {
+   let userEmail = req.body.user && req.body.user.userEmail;
+   console.log('userEmail =', userEmail);
+   let issueKey = req.body.issue && req.body.issue.key;
+   console.log('issueKey =', issueKey);
+   let issueSummary = req.body.issue && req.body.fields && req.body.fields.summary;
+   console.log('issueSummary =', issueSummary);
+   let changes = req.body.issue && req.body.changelog && req.body.changelog.items;
+   console.log('changes =', changes);
+   let change = (changes || [])[0] || {};
+   let content = `Issue ${issueKey} updated by user ${userEmail}:\n`
+    + `field "${change.field}" changed\n`
+    + `from "${change.fromString}"\n`
+    + `to "${change.toString}".\n`
+    + `Summary: ${issueSummary}`
+   console.log('content =', content);
+   request({
+    method: 'POST',
+    url: webhook,
+    json: {
+      username: 'Jira listener',
+      content: content;
+    }}, (err, ret, body) => {
+      console.log('err =', err);
+      //console.log('ret =', ret);
+      if (err) res.error(err);
+      res.status(200).send(req.body);
+    });
+ } else {
+   request({
+    method: 'POST',
+    url: webhook,
+    json: {
+      username: 'Jira listener',
+      content: 'something happen'
+    }}, (err, ret, body) => {
+      console.log('err =', err);
+      //console.log('ret =', ret);
+      if (err) res.error(err);
+      res.status(200).send(req.body);
+    });
+ }
 });
 
 app.post('/webhook', (req, res) => {
